251be2854889f6bfa939b3a4a1c6298b2dc88f4b
ddonco <donohue.d.f@gmail.com>
2020-07-24 22:12:22 -0600

    feat: Collapse nav on small screens

--- src/components/AppNav.js
+++ src/components/AppNav.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, NavLink, withRouter } from "react-router-dom"
+import { Link, NavLink, withRouter } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Form from "react-bootstrap/Form";
@@ -7,79 +7,80 @@ import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
 
 const activeStyle = {
-  color: '#17a2b8'
-}
+  color: "#17a2b8",
+};
 
 class AppNav extends React.Component {
   state = {
-    searchQuery: ''
-  }
+    searchQuery: "",
+  };
 
   handleSubmit = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     this.setState({
-      searchQuery: event.target.value
-    })
+      searchQuery: event.target.value,
+    });
 
     this.props.history.push(`/search?query=${this.state.searchQuery}&page=1`);
-  }
+  };
 
   handleChange = (event) => {
     this.setState({
-      searchQuery: event.target.value
-    })
-  }
+      searchQuery: event.target.value,
+    });
+  };
 
   render() {
-    const { searchQuery } = this.state
+    const { searchQuery } = this.state;
 
     return (
       <React.Fragment>
-        <Navbar bg="dark" variant="dark">
-          <Nav className="mr-auto">
-            <NavLink 
-              to="/"
-              exact
-              className='nav-link'
-              activeStyle={activeStyle}
-            >
-              Home
-            </NavLink>
-            <NavLink 
-              to="/archive?page=1"
-              className='nav-link'
-              activeStyle={activeStyle}
-            >
-              Archive
-            </NavLink>
-            <NavLink 
-              to="/metrics"
-              className='nav-link'
-              activeStyle={activeStyle}
-            >
-              Metrics
-            </NavLink>
-            <NavLink 
-              to="/status"
-              className='nav-link'
-              activeStyle={activeStyle}
-            >
-              Status
-            </NavLink>
-          </Nav>
-          <Form inline onSubmit={this.handleSubmit}>
-            <FormControl 
-              id="query"
-              type="text"
-              placeholder="Search" 
-              className="mr-sm-2" 
-              value={searchQuery === undefined ? '' : searchQuery}
-              onChange={this.handleChange}
-              
-            />
-            <Button variant="outline-info">
-              <Link
-                  className='search-link'
+        <Navbar expand="sm" bg="dark" variant="dark">
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <NavLink
+                to="/"
+                exact
+                className="nav-link"
+                activeStyle={activeStyle}
+              >
+                Home
+              </NavLink>
+              <NavLink
+                to="/archive?page=1"
+                className="nav-link"
+                activeStyle={activeStyle}
+              >
+                Archive
+              </NavLink>
+              <NavLink
+                to="/metrics"
+                className="nav-link"
+                activeStyle={activeStyle}
+              >
+                Metrics
+              </NavLink>
+              <NavLink
+                to="/status"
+                className="nav-link"
+                activeStyle={activeStyle}
+              >
+                Status
+              </NavLink>
+            </Nav>
+            <Form inline onSubmit={this.handleSubmit}>
+              <FormControl
+                id="query"
+                type="text"
+                placeholder="Search"
+                className="mr-sm-2"
+                value={searchQuery === undefined ? "" : searchQuery}
+                onChange={this.handleChange}
+              />
+              <Button variant="outline-info">
+                <Link
+                  className="search-link"
                   to={{
                     pathname: `/search/${searchQuery}`,
                     search: `?query=${searchQuery}&page=1`,
@@ -87,8 +88,8 @@ class AppNav extends React.Component {
                 >
                   Search
                 </Link>
-            </Button>
-            {/* <Link
+              </Button>
+              {/* <Link
               className="btn search-btn"
               to={{
                 pathname: `/search/${searchQuery}`,
@@ -97,12 +98,12 @@ class AppNav extends React.Component {
             >
               Search
             </Link> */}
-          </Form>
+            </Form>
+          </Navbar.Collapse>
         </Navbar>
-
       </React.Fragment>
     );
   }
 }
 
-export default withRouter(AppNav)
+export default withRouter(AppNav);
